91a66aed4aee46dada4334441a7358c15146f69e
Vladimir008 <vladimir.podkidyshev@gmail.com>
2022-10-18 18:47:00 +0300

    Task 8 Deleting Cyrillic and fixing the loop (part 2)

--- src/com/hillel/homeWork/lesson4/task8/WordsGame.java
+++ src/com/hillel/homeWork/lesson4/task8/WordsGame.java
@@ -9,13 +9,13 @@ public class WordsGame {
     public static void main(String[] args) {
 
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
-                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+                "garlic", "grape", "garlic", "leak", "kiwi", "melon", "mango", "mushroom", "nut", "olive", "pea", "peanut",
                 "pear", "pepper", "pineapple", "pumpkin", "potato"};
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         String randomWord = words[random.nextInt(words.length)];
-        System.out.println("Компьютер загадал слово, попробуй отгадать его!");
+        System.out.println("The computer guessed a word, try to guess it!");
         Boolean isGuessed = false; //Variable for exit from loop
         char[] guessedLetters = {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'};
 
@@ -30,20 +30,21 @@ public class WordsGame {
                 count = randomWord.length();
             }
 
-            //Loop for comparing the entered word with the guessed one and writing the guessed letters
-            for (int i = 0; i < count; i++) {
-                if (inputWord.toCharArray()[i] == randomWord.toCharArray()[i]) {
-                    guessedLetters[i] = inputWord.toCharArray()[i];
-                }
-            }
-
             //Check the guessed letters. In case, if the word is guessed we finish the loop and congratulate the winner)
             if (inputWord.equals(randomWord)) {
-                System.out.println("Поздравляю, Вы угадали слово! Это слово - " + randomWord);
+                System.out.println("Congratulations, you have guessed the word! This word - " + randomWord);
                 isGuessed = true; //Assign to a variable isGuessed value for exit from the loop. There is the end of our program
             } else {
-                System.out.println("Угаданные буквы:");
+                //Loop for comparing the entered word with the guessed one and writing the guessed letters
+                for (int i = 0; i < count; i++) {
+                    if (inputWord.toCharArray()[i] == randomWord.toCharArray()[i]) {
+                        guessedLetters[i] = inputWord.toCharArray()[i];
+                    }
+                }
+
+                System.out.println("Guessed letters:");
                 System.out.println(Arrays.toString(guessedLetters));
+
             }
         }
     }
