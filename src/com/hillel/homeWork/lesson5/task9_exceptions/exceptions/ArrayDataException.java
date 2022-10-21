e45ca716a0f8a1afddf566c1fa601addf463f16f
Vladimir008 <vladimir.podkidyshev@gmail.com>
2022-10-21 19:26:40 +0300

    Lesson 5 HW 9 exceptions modernization exceptions

--- src/com/hillel/homeWork/lesson5/task9_exceptions/exceptions/ArrayDataException.java
+++ src/com/hillel/homeWork/lesson5/task9_exceptions/exceptions/ArrayDataException.java
@@ -5,6 +5,7 @@ public class ArrayDataException extends Exception {
     public ArrayDataException(Exception e, int fieldCount, int lineCount) {
         super("Data format is incorrect in position " + "[" + fieldCount + "]" + "[" + lineCount + "]", e);
     }
+
     public ArrayDataException(int fieldCount, int lineCount) {
         super("Data format is incorrect in position " + "[" + fieldCount + "]" + "[" + lineCount + "]");
     }
