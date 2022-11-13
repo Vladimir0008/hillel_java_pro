226ea6b4d5154858a3f0a622fe93b91d6380cbcf
Vladimir008 <vladimir.podkidyshev@gmail.com>
2022-11-13 18:52:13 +0200

    Lesson 11 HW 15 fix

--- src/com/hillel/homeWork/lesson11/coffee/Main.java
+++ src/com/hillel/homeWork/lesson11/coffee/Main.java
@@ -1,20 +1,35 @@
 package com.hillel.homeWork.lesson11.coffee;
 
 import com.hillel.homeWork.lesson11.coffee.order.CoffeeOrderBoard;
+import com.hillel.homeWork.lesson11.coffee.order.exception.OrderNumberException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OrderNumberException {
         CoffeeOrderBoard coffeeOrderBoard = new CoffeeOrderBoard();
         coffeeOrderBoard.add("Alen");
         coffeeOrderBoard.add("Yoda");
         coffeeOrderBoard.add("Alena");
         coffeeOrderBoard.add("Yodar");
-
         coffeeOrderBoard.draw();
         System.out.println();
         coffeeOrderBoard.deliver(2);
         coffeeOrderBoard.draw();
-        System.out.println(coffeeOrderBoard.getLastOrder());
+        System.out.println();
+        coffeeOrderBoard.add("Dart Weider");
+        coffeeOrderBoard.add("Alen1");
+        coffeeOrderBoard.add("Yoda1");
+        coffeeOrderBoard.add("Alena1");
+        coffeeOrderBoard.add("Yodar1");
+        coffeeOrderBoard.deliver(3);
+        coffeeOrderBoard.draw();
+        System.out.println();
+        coffeeOrderBoard.deliver();
+        coffeeOrderBoard.deliver();
+        coffeeOrderBoard.draw();
+        coffeeOrderBoard.deliver();
+        coffeeOrderBoard.deliver();
+        System.out.println();
+        coffeeOrderBoard.draw();
 
 
     }
---
+++ src/com/hillel/homeWork/lesson12/practicum/Main.java
@@ -0,0 +1,13 @@
+package com.hillel.homeWork.lesson12.practicum;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {2,3,6,1,8,3,8,5,9,12};
+        Sorting sorting = new Sorting();
+        sorting.sort(array);
+        System.out.println(Arrays.toString(sorting.sort(array)));
+    }
+
+}
