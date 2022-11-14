7505a14fb2c7daf2271f657666ea36435df65130
Vladimir008 <vladimir.podkidyshev@gmail.com>
2022-11-14 20:52:54 +0200

    Lesson 10 HW 13 fix

---
+++ src/com/hillel/homeWork/lesson10/task13_lists/WordOccurrence.java
@@ -0,0 +1,19 @@
+package com.hillel.homeWork.lesson10.task13_lists;
+
+public class WordOccurrence {
+    private String name;
+    private int occurrenceCount;
+
+    public WordOccurrence(String name, int occurrenceCount) {
+        this.name = name;
+        this.occurrenceCount = occurrenceCount;
+    }
+
+    @Override
+    public String toString() {
+        return "\n{" +
+                "name='" + name + '\'' +
+                ", occurrenceCount=" + occurrenceCount +
+                "}";
+    }
+}
